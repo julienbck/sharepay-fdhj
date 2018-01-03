2b54213b37c7a4ee63312425db50ae5b7ee11eb7
hamid ben oumaizina <hamid.benoumaizina@decathlon.com>
2018-01-03 15:29:02 +0100

    changes the package json with the missing dependencies

---
+++ login.js
@@ -0,0 +1,17 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+app.use(require("body-parser").urlencoded({ extended: true }));
+app.use(require("cookie-parser")());
+app.use(
+  require("express-session")({
+    secret: "i4ms3cre7",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+// Initialize Passport and restore authentication state,
+// if any, from the session.
+app.use(passport.initialize());
+app.use(passport.session());
--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const nunjucks = require("nunjucks");
 const fetch = require("node-fetch");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
 const port = process.env.PORT || 3000;
-
 const app = express();
+
+
+
+app.use(require("body-parser").urlencoded({ extended: true }));
+app.use(require("cookie-parser")());
+app.use(
+  require("express-session")({
+    secret: "pr4sesupersecretedelamortquitue2018",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+// Initialize Passport and restore authentication state,
+// if any, from the session.
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 nunjucks.configure("views", {
   autoescape: true,
@@ -23,14 +42,39 @@ app.get("/login", function(request, result) {
   result.render("login")
 });
 
+app.post(
+  "/login",
+  passport.authenticate("local", { failureRedirect: "/login" }),
+  function(request, result) {
+    result.redirect("/profile");
+  }
+);
+
+app.get("/logout", function(request, result) {
+  request.logout();
+  result.redirect("/");
+});
+
+app.get(
+  "/profile",
+  require("connect-ensure-login").ensureLoggedIn("/login"),
+  function(request, result) {
+    result.render("profile", {
+      id: request.user.id,
+      name: request.user.displayName,
+      email: request.user.email
+    });
+});
+
+
+
+
+
+
+
+
+
 app.use(express.static(__dirname + "/public"));
-
-
-
-
-
-
-
 app.listen(port, function () {
   console.log("Server listening on port:" + port);
 });
