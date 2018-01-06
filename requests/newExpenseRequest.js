dd3f9985b84fbcbba9df88ae5dd1d31d4e63d24b
hamid ben oumaizina <hamid.benoumaizina@decathlon.com>
2018-01-06 18:21:36 +0100

    new exepense almost done

--- requests/newExpenseRequest.js
+++ requests/newExpenseRequest.js
@@ -24,21 +24,62 @@ function getAllUsersForEventId(id) {
   const client = new pg.Client();
   client.connect();
   return client.query(
-    "SELECT * FROM participants FULL JOIN events on participants.id = events.id FULL JOIN users on participants.id_user = users.id WHERE participants.id=$1",
+    "SELECT * FROM participants FULL JOIN events on participants.id_event = events.id FULL JOIN users on participants.id_user = users.id WHERE participants.id_event=$1",
     [id]
   )
   .then(response => {
-    console.log(response);
     client.end();
     return response;
   });
 }
 
+function insertNewExpenseForEventIdInSpendings(request){
+  const client = new pg.Client();
+  client.connect();
+    return client.query(
+      "select id from participants where id_event=$1 AND id_user=$2",
+      [request.params.id,request.user.id]
+  )
+  .then(response => {
+    //console.log(response.rows[0].id);
+    return client.query(
+      "INSERT INTO spending (id_participant_who_spend, description, amount, date) VALUES ($1,$2,$3,$4)",
+      [response.rows[0].id, request.body.expenseDescription, request.body.expensePrice, request.body.expenseDate]
+    )
+    client.end();
+    //console.log(response);
+  });
+
+}
+
+function insertNewExpenseDetailForEventIdInRecipients(){
+  // const client = new pg.Client();
+  // client.connect();
+  // /*
+  // 2eme insert dans recipients
+  // id depense
+  // id benificiaire
+  // */
+  // return client.query(
+  //   "INSERT INTO spending (id_participant_who_spend, description, amount, date) VALUES ($1,$2,$3,$4)",
+  //   // [event.title, event.date, event.place, '404f2252-e3a3-45fe-8c02-545475ebf37d', event.status])
+  //   [event.title, event.date, event.place, user.id, event.status])
+  // .then(response => {
+  //   client.end();
+  //   return response;
+  // });
+
+}
+
 
 //getAllUsersForEventId('0e384628-9391-460c-a51a-9eeff4f11730')
 //.then(result => console.log(result.rows));
+//http://localhost:3000/events/0f28a2ee-e169-4027-98cb-e05b716718fe/spendings/new
+
 
 module.exports = {
   getAllEvents : getAllEvents,
-  getAllUsersForEventId:getAllUsersForEventId
+  getAllUsersForEventId:getAllUsersForEventId,
+  insertNewExpenseForEventIdInSpendings:insertNewExpenseForEventIdInSpendings,
+  insertNewExpenseDetailForEventIdInRecipients:insertNewExpenseDetailForEventIdInRecipients
 };
